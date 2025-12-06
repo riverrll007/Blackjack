b8737097d301c7ae27a4948d5380778527dc70ea
riverrll007 <riverrll007@gmail.com>
2025-12-06 13:31:36 -0500

    Update Deck.py

--- Deck.py
+++ Deck.py
@@ -6,7 +6,7 @@ class Deck:
         self.num_decks = num_decks
         self.cards = []
         ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-        suits = ["Clubs, Diamonds, Hearts", "Spades"]
+        suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
         for card in range(num_decks):
             for suit in suits:
@@ -21,3 +21,4 @@ class Deck:
             return self.cards.pop()
         else:
             return None
+
