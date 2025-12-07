58e7c32d8e2b5665b0f4207fd6479f1552d71283
riverrll007 <riverrll007@gmail.com>
2025-12-07 13:27:34 -0500

    Update Deck.py

--- Deck.py
+++ Deck.py
@@ -1,14 +1,13 @@
 from Card import *
 import random
-class Deck:
-    def __init__(self, num_decks):
 
-        self.num_decks = num_decks
+class Deck:
+    def __init__(self, num_decks=1):
         self.cards = []
         ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-        suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+        suits = ["C", "D", "H", "S"]
 
-        for card in range(num_decks):
+        for _ in range(num_decks):
             for suit in suits:
                 for rank in ranks:
                     self.cards.append(Card(rank, suit))
@@ -18,7 +17,5 @@ class Deck:
 
     def draw_card(self):
         if len(self.cards) > 0:
-            return self.cards.pop()
-        else:
-            return None
-
+            return self.cards.pop() 
+        return None
